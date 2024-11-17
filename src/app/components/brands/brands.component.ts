3c4baa08fd7b6a2c258b834fef9284e9ddc9efc3
ahmedgaafer1 <midogaafer67@gmail.com>
2024-11-17 16:36:48 +0200

    update

--- src/app/components/brands/brands.component.ts
+++ src/app/components/brands/brands.component.ts
@@ -1,13 +1,13 @@
 import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
-import { BrandsService } from '../../core/services/brands.service';
-import { Ibrand } from '../../core/interfaces/ibrand';
-import { Subscription } from 'rxjs';
 import { RouterLink } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { Ibrand } from '../../core/interfaces/ibrand';
+import { BrandsService } from '../../core/services/brands.service';
 
 @Component({
   selector: 'app-brands',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink ],
   templateUrl: './brands.component.html',
   styleUrl: './brands.component.scss'
 })
